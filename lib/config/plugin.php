03167ec6cf7dd23fc224aae608569064009a0528
klxqz <klxqz@mail.ru>
2013-12-10 11:42:53 +0600

    удаление ссылки на сторонний сайт

--- lib/config/plugin.php
+++ lib/config/plugin.php
@@ -5,7 +5,7 @@ return array(
     'description' => 'Красивый эффект снега на сайте',
     'img' => 'img/newyearsnow.png',
     'vendor' => '985310',
-    'version' => '1.0.1',
+    'version' => '1.0.2',
     'rights' => false,
     'handlers' => array(
         'frontend_head' => 'frontendHead',
